dadb4297f1452587095fb117cf393308310e7569
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-16 10:56:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,9 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
+    if params[:password] != @user[:password]
+      redirect '/sessions/login'
+    end
     session[:id] = @user.id
     redirect '/users/home'
   end
